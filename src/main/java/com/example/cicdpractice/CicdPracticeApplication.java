cba3daa83faea8b806b005a6caf92372f5cb6580
BigInt1010 <tofayel.java@gmail.com>
2020-10-24 12:47:23 +0300

    adding dockerfile

--- src/main/java/com/example/cicdpractice/CicdPracticeApplication.java
+++ src/main/java/com/example/cicdpractice/CicdPracticeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class CicdPracticeApplication {
 
 	@RequestMapping("/")
